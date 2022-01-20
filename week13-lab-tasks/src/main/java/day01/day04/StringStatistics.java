8805bfc771e4079795319d2be078d9a4cce51f59
AbrihaDavid <abrihadavid@gmail.com>
2022-01-20 16:29:22 +0100

    daily task done

---
+++ week13-lab-tasks/src/main/java/day01/day04/StringStatistics.java
@@ -0,0 +1,27 @@
+package day01.day04;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class StringStatistics {
+
+    private static final String VOWELS = "aeiou";
+
+    public Map<Character,Integer> vowelCounter(String word){
+        Map<Character,Integer> result = new TreeMap<>();
+        for(int i = 0; i<word.length(); i++){
+            if(isVowel(word.charAt(i))){
+                if(!result.containsKey(word.charAt(i))){
+                    result.put(word.charAt(i),1);
+                } else {
+                    result.put(word.charAt(i),result.get(word.charAt(i))+1);
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isVowel(char c){
+        return VOWELS.indexOf(Character.toLowerCase(c)) >=0;
+    }
+}
